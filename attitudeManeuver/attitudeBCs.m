95eb0b521f5ee3d96f858daa0cdb7de5265d2085
ebabiofe <ebabiofe@purdue.edu>
2017-11-20 21:38:24 -0500

    correction in ODEs clearer expression of the BCs
    # Conflicts:
    #	attitudeManeuver/attitudeBCs.m

--- attitudeManeuver/attitudeBCs.m
+++ attitudeManeuver/attitudeBCs.m
@@ -1,6 +1,18 @@
-function [BCs, pathCs] = attitudeBCs(x0BCs, x0, xfBCs, xf, xDotFun)
 BCs = zeros(15,1);
 pathCs = zeros(2,1);
+
+if(nargin == 6)
+    collocation = 1;
+else
+    collocation = 0;
+end
+
+%% Allow for xDotFun collocation parameters
+if(collocation)
+    xDotWrapper = @(tau, x) (xDotFun(tau, x, tf));
+else
+    xDotWrapper = @(t,x) (xDotFun(t, x));
+end
 
 %% Initial state constraints
 % Reassign variables
@@ -10,7 +22,6 @@ lambda_q0 = x0(8:12);
 lambda_omega0 = x0(13:14);
 
 % Hamiltonian
-xDot = xDotFun([],x0);
 H0 = [lambda_q0;lambda_omega0]' * xDot(1:7);
 
 BCs(1:7)  = [q0-x0BCs(1:4); omega0-x0BCs(5:7)];
@@ -24,7 +35,6 @@ lambda_qf = xf(8:12);
 lambda_omegaf = xf(13:14);
 
 % Hamiltonian
-xDot = xDotFun([],xf);
 Hf = [lambda_qf;lambda_omegaf]' * xDot(1:7);
 
 BCs(8:14)  = [qf-xfBCs(1:4); omegaf-xfBCs(5:7)];
--- attitudeManeuver/attitudeODEs.m
+++ attitudeManeuver/attitudeODEs.m
@@ -11,10 +11,26 @@ lambda_q = x(8:11);
 u = control(t,x);
 
 % Compute derivatives
-qDot = 1/2 * quatmultiply([0 omega], q);
+% a. Quaternion multiplication way
+qDot = 1/2 * quatmultiply(q, [0 omega]); % check derivation
 omegaDot = u;
-lambdaDot_q = 1/2 * quatmultiply([0 omega], lambda_q);
-lambdaDot_omega = -1/2 * quatmultiply(quatconj(q), lambda_q);
+lambdaDot_q = 1/2 * quatmultiply(lambda_q, [0 omega]); % check derivation
+lambdaDot_omega = -1/2 * quatmultiply(quatconj(q), lambda_q); % check derivation
 
-dxdt = [qDot omegaDot lambdaDot_q lambdaDot_omega(2:4)]';
+% b. Element wise
+qDotEW(2) = 1/2 * ( omega(1)*q(1)   - omega(2)*q(4)     + omega(3)*q(3));
+qDotEW(3) = 1/2 * ( omega(1)*q(4)   + omega(2)*q(1)     - omega(3)*q(2));
+qDotEW(4) = 1/2 * (-omega(1)*q(3)   + omega(2)*q(2)     + omega(3)*q(1));
+qDotEW(1) = 1/2 * (-omega(1)*q(2)   - omega(2)*q(3)     - omega(3)*q(4));
+
+lambdaDot_omegaEW(1) = - 1/2 * (lambda_q(2)*q(1)    + lambda_q(3)*q(4)  - lambda_q(4)*q(3)  - lambda_q(1)*q(2));
+lambdaDot_omegaEW(2) = - 1/2 * (-lambda_q(2)*q(4)   + lambda_q(3)*q(1)  + lambda_q(4)*q(2)  - lambda_q(1)*q(3));
+lambdaDot_omegaEW(3) = - 1/2 * (lambda_q(2)*q(3)    - lambda_q(3)*q(2)  + lambda_q(4)*q(1)  - lambda_q(1)*q(4));
+
+lambdaDot_qEW(2) = -1/2 * (lambda_q(2)*0        -lambda_q(3)*omega(3)   + lambda_q(4)*omega(2)  - lambda_q(1)*omega(1));
+lambdaDot_qEW(3) = -1/2 * (lambda_q(2)*omega(3) -lambda_q(3)*0          - lambda_q(4)*omega(1)  - lambda_q(1)*omega(2));
+lambdaDot_qEW(4) = -1/2 * (-lambda_q(2)*omega(2)+lambda_q(3)*omega(1)   + lambda_q(4)*0         - lambda_q(1)*omega(3));
+lambdaDot_qEW(1) = -1/2 * (lambda_q(2)*omega(1) +lambda_q(3)*omega(2)   + lambda_q(4)*omega(3)  - lambda_q(1)*0);
+
+dxdt = [qDotEW omegaDot lambdaDot_qEW lambdaDot_omegaEW]';
 return
